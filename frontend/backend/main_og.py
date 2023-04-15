842356d1e1df563ca66ce3c2280dc0000a30f5d4
alesan99 <alesanesp99@gmail.com>
2023-04-15 10:25:54 -0500

    WIP characteristics.py

--- frontend/backend/main_og.py
+++ frontend/backend/main_og.py
@@ -1,21 +1,3 @@
-import json
-
-class characteristicsList:
-    def __init__(self, json_path):
-        with open(json_path, 'r') as f:
-            self.data = json.load(f)
-        self.category_count = len(self.data)
-
-    def getCategoryItems(self, cat_name): # category
-        return self.data[cat_name]
-    
-    def getCategoryLength(self, cat_name):
-        return len(self.getCategoryItems(cat_name))
-
-    def getCharacteristic(self, cat_name, i): # category
-        category_items = self.getCategoryItems(cat_name)
-        return category_items[i]
-
 def main():
     # u_file = str(input("What is the file? "))
     # u_exct = extractor()
