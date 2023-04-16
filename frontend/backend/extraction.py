3bbeaec4548718cec699507f71f81bc0de8d17b3
alesan99 <alesanesp99@gmail.com>
2023-04-15 21:26:33 -0500

    Updated htmls
    Co-authored-by: mcauthon471 <mcauthon471@users.noreply.github.com>
    Co-authored-by: pashiav <pashiav@users.noreply.github.com>
    Co-authored-by: cabbidge <cabbidge@users.noreply.github.com>

--- frontend/backend/extraction.py
+++ frontend/backend/extraction.py
@@ -12,16 +12,40 @@ class extractor:
 		return self.read()
 
 	def read(self):
-		s = self.input
+		s = self.input.lower()
 		self.data = []
+
+		keywords = ["midterm", "exam", "test", "due"]
 		cursor = 0
 		# print("starting scan..")
 		while cursor < self.input_len and cursor >= 0:
 			cursor = s.find('/', cursor+1, -1) # todo, use predefined key words
 			if cursor != -1:
+				# seperate the word by finding the surrounding spaces
 				space1 = s.rfind(" ", 0,cursor+1)
 				space2 = s.find(" ", cursor+1)
-				if space1 != -1 and space2 != -1 and space1 != space2 :
+
+				# see if its a date and important by finding if there are important keywords nearby
+				tolerance = 80
+				important1 = cursor-tolerance-99999
+				keyword1 = 0
+				important2 = cursor+tolerance+99999
+				keyword2 = 0
+
+				for kw in range(len(keywords)):
+					f1 = s.rfind(keywords[kw], 0,cursor+1)
+					f2 = s.find(keywords[kw], cursor+1)
+					if f1 > -1:
+						important1 = max(important1, f1)
+						keyword1 = keywords[kw]
+					if f2 > -1:
+						important2 = min(important2, f2)
+						keyword2 = keywords[kw]
+				print(important1, important2)
+
+				if space1 != -1 and space2 != -1 and space1 != space2 and ((abs(important1-cursor) < tolerance) or (abs(important2-cursor) < tolerance)):
+					dist1 = 
+					closestkeyword = 
 					self.data.append(["Midterm Day", s[space1:space2+1]])
 					# print("found at ", cursor)
 		# self.data = [["midterm time", "1/7"]]
--- frontend/backend/syllabiscan.py
+++ frontend/backend/syllabiscan.py
@@ -14,13 +14,16 @@ def scan(placeholder):
 
 # Important dates
 def display(data):
+	t1 = '<table>'
 	for i in range(len(data)):
 		# newDiv = document.createElement("div")
 		# newDiv.innerHTML = f'<p>{data[i][0]},{data[i][1]}</p>'
-		document.getElementById("location1").innerHTML = f'{data[i][0]}'
-		document.getElementById("date").innerHTML = f'{data[i][1]}'
-		document.getElementById("time").innerHTML = f'{data[i][0]},{data[i][1]}'
-		#document.body.insertBefore(newDiv, currentDiv)
+		t1 += '<tr>'
+		for i2 in range(len(data[i])):
+			t1 += f'<th>{data[i][i2]}</th>'
+		t1 += '</tr>'
+	t1 += '</table>'
+	document.getElementById("important_dates").innerHTML = t1
 
 document.getElementById("scanbutton").addEventListener("click", ffi.create_proxy(scan))
 
